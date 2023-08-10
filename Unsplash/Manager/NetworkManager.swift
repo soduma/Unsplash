33e46441496ed51e8bfd3f0e4dde579bb807b471
JANG KIHWA <soduma@naver.com>
2023-08-10 18:07:51 +0900

    some refactor

--- Unsplash/Manager/NetworkManager.swift
+++ Unsplash/Manager/NetworkManager.swift
@@ -9,26 +9,14 @@ import Foundation
 import Alamofire
 
 class NetworkManager {
-    let urlString = "https://api.unsplash.com/search/photos/?client_id=oqnMOq60UFw7nPf-1c2UDXVw0woMt00hVPQqZbmVvO0"
+    private let key = "oqnMOq60UFw7nPf-1c2UDXVw0woMt00hVPQqZbmVvO0"
     
-    // MARK: - with Async
-    func fetchWithAsync(keyword: String, page: Int) async -> Result<Unsplash, AFError> {
-        let keyword = keyword.replacingOccurrences(of: " ", with: "")
-        let url = urlString + "&query=\(keyword)&page=\(page)"
-        let data = await AF.request(url, method: .get).serializingDecodable(Unsplash.self).result
-        return data
-    }
-    
-    // MARK: - ordinary
-    func fetchImage(keyword: String, page: Int, completion: @escaping (Result<Unsplash, AFError>) -> Void) {
+    func fetchUnsplash(keyword: String, page: Int) async -> Result<Unsplash, AFError> {
+        let urlString = "https://api.unsplash.com/search/photos/?client_id=\(key)"
         let url = urlString + "&query=\(keyword)&page=\(page)"
         
-        AF.request(url, method: .get)
-            .responseDecodable(of: Unsplash.self) { response in
-                let data = response.result
-                completion(data)
-                return
-            }
-            .resume()
+        let result = await AF.request(url, method: .get)
+            .serializingDecodable(Unsplash.self).result
+        return result
     }
 }
